56305359910e26ec6790a4e36f58f9f909d2b9d0
zoudaifa <daleyzou@gmail.com>
2020-06-30 19:29:24 +0800

    Initial commit

---
+++ src/main/java/com/daleyzou/design/DesignPatternJavaApplication.java
@@ -0,0 +1,13 @@
+package com.daleyzou.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternJavaApplication.class, args);
+    }
+
+}
