433b7e2acfde33e9c0ea838cb05d5ef4702e92c6
daleyzou <daleyzou@163.com>
2020-07-01 22:06:45 +0800

    singleton

---
+++ src/main/java/com/daleyzou/design/singleton/EagerSingleton.java
@@ -0,0 +1,19 @@
+package com.daleyzou.design.singleton;
+
+/**
+ * EagerSingleton
+ * @description daleyozu
+ * @author daleyzou
+ * @date 2020年07月01日 21:32
+ * @version 1.2.0
+ */
+public class EagerSingleton {
+    static EagerSingleton eagerSingleton = new EagerSingleton();
+
+    private EagerSingleton() {
+    }
+
+    public static EagerSingleton getInstance(){
+        return eagerSingleton;
+    }
+}
---
+++ src/main/java/com/daleyzou/design/singleton/LazySingleton.java
@@ -0,0 +1,25 @@
+package com.daleyzou.design.singleton;
+
+/**
+ * LazySingleton
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年07月01日 21:38
+ * @version 1.2.0
+ */
+public class LazySingleton {
+    private volatile static  LazySingleton lazySingleton = null;
+
+    private LazySingleton() {
+    }
+    public static LazySingleton getInstance(){
+        if (lazySingleton == null){
+            synchronized (LazySingleton.class){
+                if (lazySingleton == null){
+                    lazySingleton = new LazySingleton();
+                }
+            }
+        }
+        return lazySingleton;
+    }
+}
