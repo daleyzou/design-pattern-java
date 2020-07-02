989b38b185e53a041e3db6afb03aeadf96fc2a45
daleyzou <daleyzou@163.com>
2020-07-02 22:27:30 +0800

    add adapter

---
+++ src/main/java/com/daleyzou/design/adapter/BinarySearch.java
@@ -0,0 +1,11 @@
+package com.daleyzou.design.adapter;
+
+/**
+ * BinarySearch
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年07月02日 22:27
+ * @version 1.2.0
+ */
+public class BinarySearch {
+}
---
+++ src/main/java/com/daleyzou/design/adapter/ScoreOperation.java
@@ -0,0 +1,13 @@
+package com.daleyzou.design.adapter;
+
+/**
+ * ScoreOperation
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年07月02日 22:24
+ * @version 1.2.0
+ */
+public interface ScoreOperation {
+    int[] sort(int[] array);
+    int search(int[] array, int key);
+}
