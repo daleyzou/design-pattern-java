636d9aec99054656a01424c3cfbc6e850084de47
daleyzou <daleyzou@163.com>
2020-07-10 09:44:51 +0800

    add action of chain

--- src/main/java/com/daleyzou/design/action/chain/President.java
+++ src/main/java/com/daleyzou/design/action/chain/President.java
@@ -1,5 +1,9 @@
 package com.daleyzou.design.action.chain;
 
+import com.google.gson.Gson;
+
+import java.lang.management.GarbageCollectorMXBean;
+
 /**
  * President
  * @description TODO
@@ -8,12 +12,17 @@ package com.daleyzou.design.action.chain;
  * @version 1.3.0
  */
 public class President extends Approver {
+    static final Gson GSON = new Gson();
     public President(String name) {
         super(name);
     }
 
     @Override
     public void processRequest(PurchaseRequest purchaseRequest) {
-
+        if (purchaseRequest.getAmount() >= 100000 &&purchaseRequest.getAmount() < 500000){
+            System.out.println("董事长审批：" + GSON.toJson(purchaseRequest));
+        }else {
+            this.successor.processRequest(purchaseRequest);
+        }
     }
 }
