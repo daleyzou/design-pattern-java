d5f801f42304fb46a2bc5e3e6dc92c65433aa269
daleyzou <daleyzou@163.com>
2020-09-11 09:29:08 +0800

    add facade

--- src/main/java/com/daleyzou/design/structure/facade/EncryptFacade.java
+++ src/main/java/com/daleyzou/design/structure/facade/EncryptFacade.java
@@ -7,6 +7,13 @@ package com.daleyzou.design.structure.facade;
  * @date 2020年09月10日 21:59
  * @version 1.0.0
  */
-public class EncrypyFacade {
+public class EncryptFacade {
     private FileReader fileReader;
+
+    private CipherMachine cipher;
+
+    private FileWriter fileWriter;
+
+    public void fileEncrypt(String fileNameSrc, String fileNameDes) {
+    }
 }
