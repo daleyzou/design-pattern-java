0aafaa3e3fadedf95aac9b839a8dce2b766c4ad6
daleyzou <daleyzou@163.com>
2020-10-21 22:26:28 +0800

    add command of log

---
+++ src/main/java/com/daleyzou/design/action/commend/log/Client.java
@@ -0,0 +1,11 @@
+package com.daleyzou.design.action.commend.log;
+
+/**
+ * Client
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年10月21日 22:26
+ * @version 1.0.0
+ */
+public class Client {
+}
