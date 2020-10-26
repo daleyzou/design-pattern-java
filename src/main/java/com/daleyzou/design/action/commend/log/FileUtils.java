476177dcf3f479d23f4b88cc39781524f4aad4c3
daleyzou <daleyzou@163.com>
2020-10-26 19:30:52 +0800

    add command of log

--- src/main/java/com/daleyzou/design/action/commend/log/FileUtils.java
+++ src/main/java/com/daleyzou/design/action/commend/log/FileUtils.java
@@ -1,5 +1,8 @@
 package com.daleyzou.design.action.commend.log;
 
+import java.io.*;
+import java.util.List;
+
 /**
  * FileUtils
  * @description TODO
@@ -8,4 +11,18 @@ package com.daleyzou.design.action.commend.log;
  * @version 1.0.0
  */
 public class FileUtils {
+    public void writeCommand(List<Command> commands) throws IOException {
+        FileOutputStream fileOutputStream = new FileOutputStream("config.log");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new BufferedOutputStream(fileOutputStream));
+        objectOutputStream.writeObject(commands);
+        objectOutputStream.close();
+    }
+
+    public List<Command> readCommand() throws IOException, ClassNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("config.log");
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        List<Command> commands = (List<Command>) objectInputStream.readObject();
+        objectInputStream.close();
+        return commands;
+    }
 }
