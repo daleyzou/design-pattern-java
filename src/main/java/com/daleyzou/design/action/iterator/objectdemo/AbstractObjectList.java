71e91cea1e8dc2252cb2675f32d5af4b60ff48da
daleyzou <daleyzou@163.com>
2020-10-27 09:53:45 +0800

    add Iterator

---
+++ src/main/java/com/daleyzou/design/action/iterator/objectdemo/AbstractObjectList.java
@@ -0,0 +1,11 @@
+package com.daleyzou.design.action.iterator.objectdemo;
+
+/**
+ * AbstractObjectList
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年10月27日 9:51
+ * @version 1.0.0
+ */
+public class AbstractObjectList {
+}
---
+++ src/main/java/com/daleyzou/design/action/iterator/objectdemo/ProductIterator.java
@@ -0,0 +1,11 @@
+package com.daleyzou.design.action.iterator.objectdemo;
+
+/**
+ * ProductIterator
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年10月27日 9:53
+ * @version 1.0.0
+ */
+public class ProductIterator {
+}
