9edc0ef010ffea5ead1dfe2ccd668d2848c1f356
daleyzou <daleyzou@163.com>
2020-10-28 20:36:09 +0800

    add Memento

---
+++ src/main/java/com/daleyzou/design/action/memento/Chessman.java
@@ -0,0 +1,11 @@
+package com.daleyzou.design.action.memento;
+
+/**
+ * Chessman
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年10月28日 20:34
+ * @version 1.0.0
+ */
+public class Chessman {
+}
---
+++ src/main/java/com/daleyzou/design/action/memento/MementoCaretaker.java
@@ -0,0 +1,11 @@
+package com.daleyzou.design.action.memento;
+
+/**
+ * MementoCaretaker
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年10月28日 20:35
+ * @version 1.0.0
+ */
+public class MementoCaretaker {
+}
