fabe9dc5f4a00c996b86e3c69186780e2f9319d3
daleyzou <daleyzou@163.com>
2020-10-31 12:03:46 +0800

    add observer

---
+++ src/main/java/com/daleyzou/design/action/observer/game/Client.java
@@ -0,0 +1,14 @@
+package com.daleyzou.design.action.observer.game;
+
+/**
+ * Client
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年10月31日 12:01
+ * @version 1.0.0
+ */
+public class Client {
+    public static void main(String[] args){
+
+    }
+}
--- src/main/java/com/daleyzou/design/action/observer/game/ConcreteAllyControlCenter.java
+++ src/main/java/com/daleyzou/design/action/observer/game/ConcreteAllyControlCenter.java
@@ -9,13 +9,16 @@ package com.daleyzou.design.action.observer.game;
  */
 public class ConcreteAllyControlCenter extends AllyControlCenter {
     public ConcreteAllyControlCenter(String allyName) {
+        System.out.println(allyName + "战队创建成功");
+        System.out.println("-------------------------------");
         this.allyName = allyName;
     }
 
     @Override
     public void notifyObservers(String name) {
+        System.out.println(this.allyName + "紧急通知， " + name + "受到敌人攻击");
         for (Observer player : players) {
-            if (!player.getName().equals(name)){
+            if (!player.getName().equals(name)) {
                 player.help();
             }
         }
