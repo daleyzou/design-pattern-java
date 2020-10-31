de4ec736c6a708a3a3d58865657891f04be9db65
daleyzou <daleyzou@163.com>
2020-10-31 12:07:38 +0800

    add observer

--- src/main/java/com/daleyzou/design/action/observer/game/Client.java
+++ src/main/java/com/daleyzou/design/action/observer/game/Client.java
@@ -9,6 +9,17 @@ package com.daleyzou.design.action.observer.game;
  */
 public class Client {
     public static void main(String[] args){
+        AllyControlCenter allyControlCenter = new ConcreteAllyControlCenter("金庸群侠");
+        Observer player1 = new Player("杨过");
+        Observer player2 = new Player("令狐冲");
+        Observer player3 = new Player("张无忌");
+        Observer player4 = new Player("段誉");
 
+        allyControlCenter.join(player1);
+        allyControlCenter.join(player2);
+        allyControlCenter.join(player3);
+        allyControlCenter.join(player4);
+
+        player1.beAttacked(allyControlCenter);
     }
 }
