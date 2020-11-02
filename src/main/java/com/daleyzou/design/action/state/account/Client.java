6a8168c0583aff94a41a474d0a6580be37afa683
daleyzou <daleyzou@163.com>
2020-11-02 21:00:52 +0800

    add state

--- src/main/java/com/daleyzou/design/action/state/account/Client.java
+++ src/main/java/com/daleyzou/design/action/state/account/Client.java
@@ -11,7 +11,7 @@ public class Client {
     public static void main(String[] args){
         Account account = new Account("段誉", 0.0);
         account.deposit(1000.0);
-        account.withdraw(2000.0);
+        account.withdraw(3000.0);
         account.withdraw(4000.0);
         account.withdraw(1000.0);
     }
