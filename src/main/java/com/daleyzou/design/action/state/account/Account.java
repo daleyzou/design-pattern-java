1f23dd8a6ecebdfceeb20290c2e23284769c9858
daleyzou <daleyzou@163.com>
2020-11-02 20:00:41 +0800

    add state

---
+++ src/main/java/com/daleyzou/design/action/state/account/Account.java
@@ -0,0 +1,11 @@
+package com.daleyzou.design.action.state.account;
+
+/**
+ * Account
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年11月02日 20:00
+ * @version 1.0.0
+ */
+public class Account {
+}
---
+++ src/main/java/com/daleyzou/design/action/state/account/AccountStatue.java
@@ -0,0 +1,11 @@
+package com.daleyzou.design.action.state.account;
+
+/**
+ * AccountStatue
+ * @description TODO
+ * @author daleyzou
+ * @date 2020年11月02日 19:59
+ * @version 1.0.0
+ */
+public abstract class AccountStatue {
+}
